f2509213672e9d598ad8e2b3d8cf52b899e6e2a9
Arthur Liss <artli@e1.ru>
2015-04-11 16:24:59 +0500

    Quorum

--- SimpleStorage/Client/SimpleStorageClient.cs
+++ SimpleStorage/Client/SimpleStorageClient.cs
@@ -3,36 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using Domain;
+using System.Web.Http;
 
 namespace Client
 {
     public class SimpleStorageClient : ISimpleStorageClient
     {
-        private readonly IEnumerable<string> endpoints;
+        private readonly List<string> endpoints;
+        private int nextEndpointNum = 0;
+        private string nextEndpoint {
+            get {
+                var oldNum = nextEndpointNum;
+                nextEndpointNum = (nextEndpointNum + 1) % endpoints.Count;
+                return endpoints[nextEndpointNum];
+            }
+        }
 
         public SimpleStorageClient(params string[] endpoints)
         {
             if (endpoints == null || !endpoints.Any())
                 throw new ArgumentException("Empty endpoints!", "endpoints");
-            this.endpoints = endpoints;
+            this.endpoints = endpoints.ToList();
         }
 
         public void Put(string id, Value value)
         {
-            var putUri = endpoints.First() + "api/values/" + id;
-            using (var client = new HttpClient())
-            using (var response = client.PutAsJsonAsync(putUri, value).Result)
-                response.EnsureSuccessStatusCode();
+            while (true) {
+                var putUri = nextEndpoint + "api/values/" + id;
+                using (var client = new HttpClient())
+                using (var response = client.PutAsJsonAsync(putUri, value).Result)
+                    try {
+                        response.EnsureSuccessStatusCode();
+                    } catch (HttpRequestException) {
+                        continue;
+                    }
+                break;
+            }
         }
 
         public Value Get(string id)
         {
-            var requestUri = endpoints.First() + "api/values/" + id;
-            using (var client = new HttpClient())
-            using (var response = client.GetAsync(requestUri).Result)
-            {
-                response.EnsureSuccessStatusCode();
-                return response.Content.ReadAsAsync<Value>().Result;
+            while (true) {
+                var requestUri = nextEndpoint + "api/values/" + id;
+                using (var client = new HttpClient())
+                using (var response = client.GetAsync(requestUri).Result) {
+                    try {
+                        response.EnsureSuccessStatusCode();
+                    } catch (HttpRequestException) {
+                        continue;
+                    }
+                    return response.Content.ReadAsAsync<Value>().Result;
+                }
             }
         }
     }
--- SimpleStorage/SimpleStorage/Controllers/ValuesController.cs
+++ SimpleStorage/SimpleStorage/Controllers/ValuesController.cs
@@ -2,6 +2,10 @@
 using System.Web.Http;
 using Domain;
 using SimpleStorage.Infrastructure;
+using Client;
+using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace SimpleStorage.Controllers
 {
@@ -10,12 +14,16 @@ namespace SimpleStorage.Controllers
         private readonly IConfiguration configuration;
         private readonly IStateRepository stateRepository;
         private readonly IStorage storage;
+        private readonly List<InternalClient> clients;
 
         public ValuesController(IStorage storage, IStateRepository stateRepository, IConfiguration configuration)
         {
             this.storage = storage;
             this.stateRepository = stateRepository;
             this.configuration = configuration;
+            clients = configuration.OtherShardsPorts
+                .Select(port => new InternalClient("http://127.0.0.1:" + port.ToString() + "/"))
+                .ToList();
         }
 
         private void CheckState()
@@ -24,21 +32,60 @@ namespace SimpleStorage.Controllers
                 throw new HttpResponseException(HttpStatusCode.InternalServerError);
         }
 
+        private IEnumerable<InternalClient> MixClients() {
+            var rnd = new Random();
+            return clients.OrderBy(client => rnd.NextDouble());
+        }
+
+        private Value GetLocal(string id) {
+            CheckState();
+            return storage.Get(id);
+        }
+
         // GET api/values/5 
         public Value Get(string id)
         {
-            CheckState();
-            var result = storage.Get(id);
-            if (result == null)
+            var quorum = clients.Count / 2; 
+            var results = new List<Value>();
+            results.Add(GetLocal(id));
+            foreach (var client in MixClients()) {
+                if (results.Count >= quorum)
+                    break;
+                try {
+                    results.Add(client.Get(id));
+                } catch (HttpResponseException e) {
+                    if (e.Response.StatusCode == HttpStatusCode.NotFound)
+                        results.Add(null);
+                }
+            }
+            if (results.Count < quorum)
                 throw new HttpResponseException(HttpStatusCode.NotFound);
-            return result;
+            var bestAnswer = results.OrderBy(result => result == null ? long.MinValue : result.Revision).ElementAt(0);
+            return bestAnswer;
+        }
+
+        private void PutLocal(string id, Value value) {
+            CheckState();
+            storage.Set(id, value);
         }
 
         // PUT api/values/5
         public void Put(string id, [FromBody] Value value)
         {
-            CheckState();
-            storage.Set(id, value);
+            var successfulAnswersRemaining = clients.Count / 2;
+            PutLocal(id, value);
+            foreach (var client in MixClients()) {
+                if (successfulAnswersRemaining <= 0)
+                    break;
+                try {
+                    client.Put(id, value);
+                } catch (HttpResponseException) {
+                    continue;
+                }
+                successfulAnswersRemaining--;
+            }
+            if (successfulAnswersRemaining > 0)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
         }
     }
 }
